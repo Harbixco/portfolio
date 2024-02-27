9b266ba8a4422fa21ca7c258af92f164b1054026
hagios <agbajeabiodunkay@gmail.com>
2024-02-27 13:50:26 +0100

    new modification

--- src/Component/Skills/index.tsx
+++ src/Component/Skills/index.tsx
@@ -2,29 +2,32 @@ import React from "react";
 import AnimatedPage from "../Animated/AnimatedPage";
 import { data, image } from "../Utils/data.tsx";
 
-export default function Index() {
+type Colors = {
+  blackColor: React.CSSProperties;
+  LightBlue: React.CSSProperties;
+};
+
+export default function Index(props: Colors) {
   return (
     <AnimatedPage>
       <div className="text-2xl font-medium mx-5">
-        The technologies I'm currently using are:
+        The technologies I'm currently using are :
       </div>
-      <div>
-        <div className=" gap-2 group sm:gap-4 px-5 w-[100%] items-center justify-center grid grid-cols-4">
+      <div className="grid grid-cols-1 lg:grid-cols-2 ">
+        <div style={props.blackColor} className="grid gap-2 grid-cols-3">
           {data.map((data, index) => {
             return (
-              <div
-                key={index}
-                className="bg-tertiary p-2 sm:p-4 text-center rounded-sm"
-              >
-                <img src={data.img} alt="" className="" />
-                <h1 className="">{data.name}</h1>
+              <div className="border-solid my-1 border-2 py-3 text-center" style={props.LightBlue} key={index}>
+                <div className="flex justify-center ">
+                  <img src={data.img} alt="" className="mx-"  />
+                </div>
+                <div className="text-white">{data.name}</div>
               </div>
             );
           })}
         </div>
-
         <div>
-          <img src={image.slide} alt="" />
+          <img src={image.slide} alt="" className="hidden lg:block" />
         </div>
       </div>
     </AnimatedPage>
