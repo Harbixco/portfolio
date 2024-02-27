6974d03d9d2794c28ffff5f2911d3baf886c16e0
hagios <agbajeabiodunkay@gmail.com>
2024-02-27 14:03:05 +0100

    skills modification

--- src/Component/Skills/index.tsx
+++ src/Component/Skills/index.tsx
@@ -10,16 +10,16 @@ type Colors = {
 export default function Index(props: Colors) {
   return (
     <AnimatedPage>
-      <div className="text-2xl font-medium mx-5">
+       <div className="text-2xl font-medium py-3 pl-2 text-white" style={props.blackColor} >
         The technologies I'm currently using are :
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 ">
         <div style={props.blackColor} className="grid gap-2 grid-cols-3">
           {data.map((data, index) => {
             return (
-              <div className="border-solid my-1 border-2 py-3 text-center" style={props.LightBlue} key={index}>
+              <div className="border-solid my-1 border-2 py-1 mx-3 lg:mx-0 text-center" style={props.LightBlue} key={index}>
                 <div className="flex justify-center ">
-                  <img src={data.img} alt="" className="mx-"  />
+                  <img src={data.img} alt=""/>
                 </div>
                 <div className="text-white">{data.name}</div>
               </div>
