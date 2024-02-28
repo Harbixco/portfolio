dd607df0ec7267cd0375c15d9129e3a504437f6a
hagios <agbajeabiodunkay@gmail.com>
2024-02-28 21:54:59 +0100

    made some changes in skills and contact components

--- src/Component/Education/index.tsx
+++ src/Component/Education/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import AnimatedPage from "../Animated/AnimatedPage";
+import file from "../../Images/file.pdf";
 
 export default function Index() {
-
-
-  return <AnimatedPage></AnimatedPage>;
+  return (
+    <AnimatedPage>
+      <a href="../../Images/file.pdf" download={file}>
+        Download
+      </a>
+    </AnimatedPage>
+  );
 }
