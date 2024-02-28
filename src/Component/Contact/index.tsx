d1e32f2506a3ae072c8d6ce39f6cb9ac89d70097
hagios <agbajeabiodunkay@gmail.com>
2024-02-28 09:58:35 +0100

    made some changes in skills and contact components

--- src/Component/Contact/index.tsx
+++ src/Component/Contact/index.tsx
@@ -2,83 +2,75 @@ import React from "react";
 import FillForm from "./FillForm";
 import AnimatedPage from "../Animated/AnimatedPage";
 
-type ContactProps = {
-  blackColor: React.CSSProperties;
-  LightBlue: React.CSSProperties;
-};
-
-export default function Index(props: ContactProps) {
+export default function Index() {
   return (
-    <AnimatedPage>
-      <div className="container-fluid" style={props.blackColor}>
-        <div className="row">
-          <div className="col-md-3 offset-md-2 text-center">
-            <div style={props.LightBlue} className="text-3xl mt-5">
-              {" "}
-              Get in{" "}
-            </div>
-            <div style={props.LightBlue} className="text-3xl my-2">
-              {" "}
-              Touch{" "}
-            </div>
-            <div className="text-white text-justify my-4">
-              I am available on almost every social media. You can message me, I
-              will definitely reply as fast as possible. I can help you you with
-              Web Development.
-            </div>
-            <div className=" flex justify-around my-7">
-              <div>
-                <a
-                  href="https://agbajeabiodunkay@gmail.com"
-                  className="icon-[mdi--gmail] text-3xl text-white"
-                ></a>
-              </div>
-              <div>
-                <a
-                  href="https://github.com/Harbixco"
-                  className="icon-[bi--github] text-3xl text-white"
-                ></a>
-              </div>
-              <div>
-                <a
-                  href="https://x.com/KayusdMan?t=OjQWGO6eRnsqkFmSgWVKZQ&s=08 "
-                  className="icon-[ri--twitter-x-line] text-3xl text-white"
-                ></a>
-              </div>
-              <div>
-                <a
-                  href="https://wa.me/+2349039415354?text=Hello,+I+want+to+give+you+a+project."
-                  className="icon-[ic--baseline-whatsapp] text-3xl text-white"
-                ></a>
-              </div>
-              <div>
-                <a
-                  href="https://www.linkedin.com/in/agbaje-abiodun/"
-                  className="icon-[akar-icons--linkedin-box-fill] text-3xl text-white"
-                ></a>
-              </div>
-            </div>
-          </div>
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div>
+        <div className="text-3xl bg-white inknut-antiqua-medium text-center my-3">
+          Get in Touch
+        </div>
 
-          <div className="col-md-3 offset-md-1">
-            <div className="text-center">
-              {" "}
-              <div style={props.LightBlue} className="md:my-10 text-2xl">
-                TALK TO ME
-              </div>
-              <div className="text-white">
-                Do you have a project for me or a job?
-              </div>
-              <div className="text-white">
-                Feel free to contact me by filing the form below
-              </div>
-              <div>
-                <FillForm />
-              </div>
+        <div className="bg-black">
+          <div className="text-white text-justify mx-3 py-5">
+            I am available on almost every social media. You can message me, I
+            will definitely reply as fast as possible. I can help you you with
+            Web Development.
+          </div>
+          <div className=" flex justify-around py-24">
+            <div>
+              <a
+                href="https://agbajeabiodunkay@gmail.com"
+                className="icon-[mdi--gmail] text-3xl text-white"
+              ></a>
+            </div>
+
+            <div>
+              <a
+                href="https://github.com/Harbixco"
+                className="icon-[bi--github] text-3xl text-white"
+              ></a>
+            </div>
+
+            <div>
+              <a
+                href="https://x.com/KayusdMan?t=OjQWGO6eRnsqkFmSgWVKZQ&s=08 "
+                className="icon-[ri--twitter-x-line] text-3xl text-white"
+              ></a>
+            </div>
+
+            <div>
+              <a
+                href="https://wa.me/+2349039415354?text=Hello,+I+want+to+give+you+a+project."
+                className="icon-[ic--baseline-whatsapp] text-3xl text-white"
+              ></a>
+            </div>
+
+            <div>
+              <a
+                href="https://www.linkedin.com/in/agbaje-abiodun/"
+                className="icon-[akar-icons--linkedin-box-fill] text-3xl text-white"
+              ></a>
             </div>
           </div>
         </div>
       </div>
-    </AnimatedPage>
+
+      <div>
+        <div className="text-3xl bg-white inknut-antiqua-medium text-center my-3">
+          Talk to me
+        </div>
+        <div className="bg-black">
+          <div className="text-white text-center pt-5">
+            Do you have a project for me or a job?
+          </div>
+          <div className="text-white text-center py-3">
+            Feel free to contact me by filing the form below
+          </div>
+          <div>
+            <FillForm />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
