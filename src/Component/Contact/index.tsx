02aa1c1dfa2c23d3e0c12544cd898ad97af0bcb9
hagios <agbajeabiodunkay@gmail.com>
2024-05-02 15:08:18 +0100

    made changes in Fillform and Header components

--- src/Component/Contact/index.tsx
+++ src/Component/Contact/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import FillForm from "./FillForm.jsx";
+import FillForm from "./FillForm.jsx";
 import AnimatedPage from "../Animated/AnimatedPage";
 
 type Colors = {
@@ -10,14 +10,17 @@ type Colors = {
 export default function Index(props: Colors) {
   return (
     <AnimatedPage>
-      <div>
-        <div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 dark:bg-black bg-white">
+        <div className="sm:pb-0 pb-5">
+          <FillForm />
+        </div>
+        <div className="">
           <div className="text-3xl inknut-antiqua-medium text-center py-1 dark:text-white text-blue-600 font-semibold">
             Get in Touch
           </div>
 
-          <div style={props.LightBlue}>
-            <div className="text-white text-justify mx-3 py-5">
+          <div className="dark:text-white text-blue-600">
+            <div className="text-justify mx-3 py-5">
               I am available on almost every social media. You can message me, I
               will definitely reply as fast as possible. I can help you you with
               Web Development.
@@ -26,35 +29,35 @@ export default function Index(props: Colors) {
               <div>
                 <a
                   href="https://agbajeabiodunkay@gmail.com"
-                  className="icon-[mdi--gmail] text-3xl text-white"
+                  className="icon-[mdi--gmail] text-3xl"
                 ></a>
               </div>
 
               <div>
                 <a
                   href="https://github.com/Harbixco"
-                  className="icon-[bi--github] text-3xl text-white"
+                  className="icon-[bi--github] text-3xl"
                 ></a>
               </div>
 
               <div>
                 <a
                   href="https://x.com/KayusdMan?t=OjQWGO6eRnsqkFmSgWVKZQ&s=08 "
-                  className="icon-[ri--twitter-x-line] text-3xl text-white"
+                  className="icon-[ri--twitter-x-line] text-3x1"
                 ></a>
               </div>
 
               <div>
                 <a
                   href="https://wa.me/+2349039415354?text=Hello,+I+want+to+give+you+a+project."
-                  className="icon-[ic--baseline-whatsapp] text-3xl text-white"
+                  className="icon-[ic--baseline-whatsapp] text-3xl"
                 ></a>
               </div>
 
               <div>
                 <a
                   href="https://www.linkedin.com/in/agbaje-abiodun/"
-                  className="icon-[akar-icons--linkedin-box-fill] text-3xl text-white"
+                  className="icon-[akar-icons--linkedin-box-fill] text-3xl"
                 ></a>
               </div>
             </div>
--- src/Component/Header/index.tsx
+++ src/Component/Header/index.tsx
@@ -27,7 +27,7 @@ export default function Example(props: HeaderStyle) {
         <div className="">
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className=" relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="absolute -inset-0.5" />
@@ -38,10 +38,10 @@ export default function Example(props: HeaderStyle) {
                   )}
                 </Disclosure.Button>
               </div>
-              <div className=" flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
+              <div className=" flex sm:justify-start">
+                <div className="">
                   <img
-                    className="h-8 w-auto px-36"
+                    className="h-8 w-auto pr-44 pl-5"
                     src={logo}
                     alt="Your Company"
                   />
@@ -67,7 +67,6 @@ export default function Example(props: HeaderStyle) {
                 </div>
 
                 <DarkMode />
-
               </div>
             </div>
           </div>
